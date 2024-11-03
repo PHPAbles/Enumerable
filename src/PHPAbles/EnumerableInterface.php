05f2161e72a3217a0b416315e533a1d8313d6aad
Jay Potter <j@ypotter.ca>
2024-11-03 15:53:41 -0500

    re-namespace

--- src/PHPAbles/EnumerableInterface.php
+++ src/PHPAbles/EnumerableInterface.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace PHPAbles\Enumerable;
+namespace PHPAbles;
 
 use PHPAbles\{Arrayable\ArrayableInterface, Countable\CountableInterface};
-use PHPAbles\Json\{JsonableInterface, Serializable\JsonSerializableInterface};
+use PHPAbles\Json\{JsonableInterface, JsonSerializableInterface};
 use Potter\Iterator\Aggregate\IteratorAggregateInterface,
     Illuminate\Support\Enumerable;
 
