913e3a5b95b82d3a3002216befa1b7682acf9e60
Jay Potter <j@ypotter.ca>
2024-11-19 21:24:14 -0500

    use PHPAbles\Arrayable\ArrayableInterface

--- src/PHPAbles/EnumerableInterface.php
+++ src/PHPAbles/EnumerableInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PHPAbles;
 
 use Illuminate\Support\Enumerable, Potter\Iterator\Aggregate\IteratorAggregateInterface;
+use PHPAbles\Arrayable\ArrayableInterface;
 use PHPAbles\Json\{JsonableInterface, JsonSerializableInterface};
 
 interface EnumerableInterface extends Enumerable, ArrayableInterface, 
